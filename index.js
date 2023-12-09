7b2a7f083d475729f3aa68acf56aff305396d92a
hongDev <senghonghang@gmail.com>
2023-12-09 23:10:42 +0700

    new updated

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const Token = "67fb1f9b0dd6a3e292e1fa4c9752d1af3f1ea0a787ca95f698f5b8883af5d40d";
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
-
-app.get('/plan', async (req, res) => {
+app.get('/', async (req, res) => {
     axios.get('https://api.qase.io/v1/plan/RPP?limit=100&offset=0', {
         headers: {
             Token: Token,
@@ -43,8 +39,8 @@ app.post('/add', (req, res) => {
             res.status(500).send('Error');
         });
 })
-// app.listen(3000, () => {
-//     console.log(`http://localhost:3000`);
-// })
+app.listen(3000, () => {
+    console.log(`http://localhost:3000`);
+})
 
 
