454511bfca7f2981cc220587d9634b38556a736f
hongDev <80942516+hongDevF@users.noreply.github.com>
2023-12-10 22:35:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.post('/add', (req, res) => {
 app.get('/author', (req, res) => {
     res.status(200).json({
         data: {
-            Senghong: "67fb1f9b0dd6a3e292e1fa4c9752d1af3f1ea0a787ca95f698f5b8883af5d40d",
             Sopha: "38c77aaeb9d5e0dfcaf99627a74e70949b3e668ad2fed73a1845bd6ac7ae7f22",
+            Senghong: "67fb1f9b0dd6a3e292e1fa4c9752d1af3f1ea0a787ca95f698f5b8883af5d40d",
             Sovannak: "",
             Baros: "",
             Saven: "",
@@ -63,5 +63,6 @@ app.get('/env', (req, res) => {
 app.listen(3000, () => {
     console.log(`http://localhost:3000`);
 })
+
 
 
